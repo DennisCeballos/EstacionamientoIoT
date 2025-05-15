cf91f0d656e0d3c5af0c385490af5ae0f2c45450
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2025-05-14 19:21:16 -0500

    feat. Version Base de codigo

---
+++ base_de_tf-1/base_de_tf.ino
@@ -0,0 +1,68 @@
+/*
+ * Configurar Pines
+*/
+// Pines de Multiplexor Analogico
+/* Requiere: 4 digital
+*/
+
+// Pines de Multiplexor Digital
+/* Requiere: 
+*/
+
+// Pines de "solo un" sensor ultrasonico
+/* Requiere: 2 analogicos
+*/
+
+// Pines de input Boton
+/* Requiere:
+*/
+
+// Pines de input sensor IR
+/* Requiere:
+*/
+
+// Pines de servo motor
+/* Requiere:
+*/
+
+// Variables del sistema
+/* 
+*/
+
+// Setup de variables
+void setup() {
+  // put your setup code here, to run once:
+}
+
+
+void loop() {
+  //
+  // INSPECCIONAR TODOS LOS SENSORES ULTRASONICOS
+  //
+  // Recorrer la lista de multiplexores
+  // Por cada elemento del multiplexor, medir la distancia
+
+  // Actualizar la lista del "ESTADO"(Ocupado, Desocupado) de los slots
+
+
+  // Actualizar estado de los pines
+  // Por cada elemento de la lista del "ESTADO" de los slots
+
+  // Actualizar el valor del LED correspondiente
+
+
+  //
+  // SUBIR INFORMACIÓN AL SERVIDOR
+  //
+  // Enviar al servidor la lista del "ESTADO" de los slots
+
+  //
+  // Actualizar informacion en el LCD
+  //
+
+
+  //
+  // VERIFICAR SI ALGUIEN "TOCA A LA PUERTA"
+  //
+  // IMPLEMENTACION DE AUTOMATA DE ESTADOS FINITOS
+}
