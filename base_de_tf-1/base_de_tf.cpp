6f59b49c7d8a8c084e84604762316cfb1cf218be
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2025-05-16 17:56:21 -0500

    feat. variables globales para ultrasonicos

---
+++ base_de_tf-1/base_de_tf.cpp
@@ -0,0 +1,34 @@
+enum Estado
+{
+    ESPERA,
+    CONSULTA,
+    APERTURA,
+    CIERRE
+};
+
+Estado estado;
+
+int automata() {
+
+    enum Estado estado;
+    estado = ESPERA;
+
+    switch (estado)
+    {
+    case ESPERA:
+        
+        break;
+    case CONSULTA:
+        
+        break;
+    case APERTURA:
+        
+        break;
+    case CIERRE:
+        
+        break;
+    
+    default:
+        break;
+    }
+}
