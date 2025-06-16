9f885f179e70931013801c58ae88e93266ace5d3
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2025-06-15 20:05:05 -0500

    feat. Archivo de componentes del proyecto

---
+++ arduinoUNO/componentes.h
@@ -0,0 +1,172 @@
+/* --- componentes.h --- */
+/*
+Clase para definir todos los componentes para el proyecto
+*/
+#ifndef COMPONENTES_H
+#define COMPONENTES_H
+
+// Estructura para controlar un LED
+typedef struct LED
+{
+    int pin;
+
+    LED() {}
+    LED(int _pin) : pin(_pin)
+    {
+        pinMode(pin, OUTPUT);
+    }
+
+    void encender() const
+    {
+        digitalWrite(pin, HIGH);
+    }
+
+    void apagar() const
+    {
+        digitalWrite(pin, LOW);
+    }
+
+} LED;
+
+// Estructura de un sensor ultrasonico
+typedef struct SensorUltrasonico
+{
+    int triggerPin;
+    int echoPin;
+
+    SensorUltrasonico(int _triggerPin, int _echoPin) : triggerPin(_triggerPin), echoPin(_echoPin)
+    {
+        // definir los pinmode del sensor
+        pinMode(triggerPin, OUTPUT);
+        pinMode(echoPin, INPUT);
+    }
+
+    int medirDistancia() const
+    {
+        // limpiar sensor (?)
+        digitalWrite(triggerPin, LOW);
+        delayMicroseconds(2);
+        // emitir onda ultrasonido
+        digitalWrite(triggerPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(triggerPin, LOW);
+
+        // medir con echoPIN la duracion
+        int duracion = pulseIn(echoPin, HIGH);
+
+        // medida de veolcidad del sonido (cm/micro s)
+        const float velocidadSonido = 0.03446f;
+        // medida de distancia en cm
+        const int distancia = duracion * velocidadSonido / 2;
+
+        // ? rando maximo de un sensor ultrasonico 4 metros (opcional?)
+        // if (distancia >= 180) {
+        //   return -1;
+        // }
+        return distancia;
+    }
+
+} SensorUltrasonico;
+
+// Estructura de un Multiplexor con 8 pines de entrada DIGITAL
+typedef struct Multiplexor
+{
+    const int *controlPins; // S0, S1, S2
+    int signalPin;          // salida SIG (o tambien el OUTPUT)
+
+    Multiplexor(const int *_controlPins, int _signalPin) : controlPins(_controlPins), signalPin(_signalPin)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            pinMode(controlPins[i], OUTPUT);
+        }
+    }
+
+    void seleccionarEntrada(int pos_entrada)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            // la operacion de pos_entrada >> 1 & 1 obtiene el valor en bits del numero segun su posicion i
+            digitalWrite(controlPins[i], (pos_entrada >> i) & 1);
+        }
+    }
+
+    int leerEntrada(int entrada)
+    {
+        seleccionarEntrada(entrada);
+        return digitalRead(signalPin);
+    }
+} Multiplexor;
+
+// Estructura de un Demultiplexor con 8 pines de salida (3 control pins) DIGITAL
+typedef struct Demultiplexor
+{
+    const int *controlPins; // S0, S1, S2
+    int inputPin;           // Pin de entrada para ser redirigido
+    int enablePin = -1;     // probablemente necesario
+
+    Demultiplexor(const int *_controlPins, int _inputPin) : controlPins(_controlPins), inputPin(_inputPin)
+    {
+        // Inicializar los pines
+        for (int i = 0; i < 3; ++i)
+        {
+            pinMode(controlPins[i], OUTPUT);
+        }
+        pinMode(inputPin, OUTPUT);
+    }
+
+    void seleccionarSalida(int pos_salida)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            // la operacion de pos_salida >> 1 & 1 obtiene el valor en bits del numero segun su posicion i
+            digitalWrite(controlPins[i], (pos_salida >> i) & 1);
+        }
+    }
+
+    void enviarASalida(int salida, int valor)
+    {
+        seleccionarSalida(salida);
+        digitalWrite(inputPin, valor);
+    }
+} Demultiplexor;
+
+// Estructura de un ShiftRegister con 8 pines de salida
+struct ShiftRegister {
+    int dataPin;
+    int clockPin;
+    int latchPin;
+    int value = 0;
+
+    ShiftRegister(int data, int clock, int latch)
+        : dataPin(data), clockPin(clock), latchPin(latch)
+    {
+        pinMode(dataPin, OUTPUT);
+        pinMode(clockPin, OUTPUT);
+        pinMode(latchPin, OUTPUT);    
+    }
+
+    // Envia el valor val en binario sobre la salida (val debe ser el entero decimal)
+    void escribir(int val) {
+        digitalWrite(latchPin, LOW);
+        shiftOut(dataPin, clockPin, MSBFIRST, val);
+        digitalWrite(latchPin, HIGH);
+    }
+
+    // Definir el estado de un bit en especifico en el ShiftRegister
+    void setBit(int bitIndex, bool valor) {
+        if (bitIndex > 7) return;
+        if (valor)
+            value |= (1 << bitIndex);
+        else
+            value &= ~(1 << bitIndex);
+        escribir(value);
+    }
+
+    void limpiar() {
+        escribir(0);
+    }
+};
+
+
+#endif // COMPONENTES_H
